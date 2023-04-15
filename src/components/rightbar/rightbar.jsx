5c9320445cccfba58d2d8d560c60811fdeed56a3
bwalidd <bouwachmawalid1@gmail.com>
2023-04-15 18:25:50 +0000

    rightbar

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -5,7 +5,25 @@ class Rightbar extends Component {
     render() {
         return(
             <div className="rightbarcontainer">
-                right
+                <div className="wrapper">
+                     <div className="birthdaycontainer">
+                        <img className="birthdayimg" src="/assets/gift.png" alt="" />
+                        <span className="birthdaytext">
+                            <b>Paulo Henrique</b> and <b>3 other person</b> have a birthday today.
+                        </span>
+                     </div>
+                        <img className="rightbarad" src="/assets/ad.png" alt="" />
+                        <h4 className="rightbarTitle">Online Friends</h4>
+                        <ul className="rightbarFriendList">
+                            <li className="rightbarFriend">
+                                <div className="rightbarProfileImgContainer">
+                                    <img className="rightbarProfileImg" src="/assets/person/3.jpeg" alt="" />
+                                    <span className="rightbarOnline"></span>
+                                </div>
+                                <span className="profilename"> Anna lora</span>
+                            </li>
+                        </ul>
+                </div>
             </div>
         )
     }
